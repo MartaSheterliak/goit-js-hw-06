ecc684b7bbe1a845fa2613e62948800188ebcc4b
MartaSheterliak <m.sheterliak@ukr.net>
2023-01-16 14:18:40 +0200

    Домашня робота 6 перероблена

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,15 @@
-const categoryList = document.querySelectorAll("#categories .item");
-
-console.log(`Namber of categories: ${categoryList.length}`);
-
-categoryList.forEach((element) => {
-    console.log(`Category: ${element.querySelector("h2").textContent}`);
-    console.log(`Elements: ${element.querySelectorAll("h2 + ul > li").length}`); 
-});
+const counter = parent => { 
+    return parent.childElementCount 
+};
+const message = elements => {
+    elements.forEach(element => {
+        const firstEl = element.firstElementChild;
+        const secondElem = firstEl.nextElementSibling
+        console.log(`Category: ${firstEl.textContent}`);
+        console.log(`Elements: ${counter(secondElem)}`);
+    });
+};
+const categories = document.querySelector('#categories');
+const subCategories = categories.querySelectorAll('.item');
+console.log(`Number of categories: ${counter(categories)}`);
+message(subCategories);
